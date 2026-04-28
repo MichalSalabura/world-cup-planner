5de4f51f3940a3bf529991e9f8c87ffc4d2549a6
MichalS <D00282723@student.dkit.ie>
2026-04-28 23:41:40 +0100

    feat: fetch map

---
+++ client/src/components/ui/Map.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from "react";
+
+import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+
+const libraries = ["places"];
+
+const Map = () => {
+    const [places, setPlaces] = useState(null);
+
+    const { isLoaded } = useJsApiLoader({
+        googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+        libraries,
+    });
+
+    useEffect(() => {
+        if (!isLoaded) return;
+        if (places) return;
+
+        const service = new window.google.maps.places.PlacesService(
+            document.createElement("div"),
+        );
+
+        service.nearbySearch(
+            {
+                location: { lat: 53.3498, lng: -6.2603 },
+                radius: 1000,
+                type: "tourist_attraction",
+            },
+            (results, status) => {
+                if (
+                    status === window.google.maps.places.PlacesServiceStatus.OK
+                ) {
+                    setPlaces(results);
+                }
+            },
+        );
+    }, [isLoaded]);
+
+    if (!isLoaded) return <div>Loading...</div>;
+
+    return (
+        <GoogleMap
+            mapContainerStyle={{ width: "100%", height: "500px" }}
+            center={{ lat: 53.3498, lng: -6.2603 }}
+            zoom={12}
+        />
+    );
+};
+
+export default Map;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,15 @@
 import React from "react";
+import Map from "../components/ui/Map";
+import "../styles/Home.css";
 
 export const Home = (props) => {
-    return <div>Hello World!</div>;
+    return (
+        <div class="ms_container">
+            <div id="ms_mapContainer">
+                <Map />
+            </div>
+        </div>
+    );
 };
 
 export default Home;
