af043dd1fd920b072c1099984d21581434504814
MichalS <D00282723@student.dkit.ie>
2026-04-29 22:59:07 +0100

    fix: remove router

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home.js";
 import Header from "./components/layout/Header.js";
 import "./styles/App.css";
@@ -7,16 +6,12 @@ import "./styles/App.css";
 export const App = (props) => {
     const [country, setCountry] = useState("USA");
     return (
-        <BrowserRouter>
-            <div className="ms_appContainer">
-                <Header setCountry={setCountry} />
-                <div className="ms_pageContent">
-                    <Routes>
-                        <Route path="/" element={<Home currentCountry={country} />} />
-                    </Routes>
-                </div>
+        <div className="ms_appContainer">
+            <Header setCountry={setCountry} />
+            <div className="ms_pageContent">
+                <Home currentCountry={country} />
             </div>
-        </BrowserRouter>
+        </div>
     );
 };
 
