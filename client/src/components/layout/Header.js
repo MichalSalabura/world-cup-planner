0f5e5bccde21efb4bca9dee4a1743222e168c58e
MichalS <D00282723@student.dkit.ie>
2026-04-30 02:05:24 +0100

    style: header

--- client/src/components/layout/Header.js
+++ client/src/components/layout/Header.js
@@ -1,25 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../css/Header.css";
 
 const Header = ({ setCountry }) => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const handleCountrySelect = (country) => {
+        setCountry(country);
+        setMenuOpen(false);
+    };
+
     return (
         <div className="ms_headerContainer">
-            <ul className="ms_countryList">
+            <button
+                className="ms_hamburger"
+                onClick={() => setMenuOpen(!menuOpen)}
+                aria-label="Toggle menu"
+            >
+                <span
+                    className={`ms_hamburgerLine ${menuOpen ? "ms_hamburgerOpen" : ""}`}
+                ></span>
+                <span
+                    className={`ms_hamburgerLine ${menuOpen ? "ms_hamburgerOpen" : ""}`}
+                ></span>
+                <span
+                    className={`ms_hamburgerLine ${menuOpen ? "ms_hamburgerOpen" : ""}`}
+                ></span>
+            </button>
+            <ul
+                className={`ms_countryList ${menuOpen ? "ms_countryListOpen" : ""}`}
+            >
                 <li
                     className="ms_countryListItem"
-                    onClick={() => setCountry("USA")}
+                    onClick={() => handleCountrySelect("USA")}
                 >
                     USA
                 </li>
                 <li
                     className="ms_countryListItem"
-                    onClick={() => setCountry("Canada")}
+                    onClick={() => handleCountrySelect("Canada")}
                 >
                     Canada
                 </li>
                 <li
                     className="ms_countryListItem"
-                    onClick={() => setCountry("Mexico")}
+                    onClick={() => handleCountrySelect("Mexico")}
                 >
                     Mexico
                 </li>
